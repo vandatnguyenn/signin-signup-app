2064c813ebc98231a80cb2f657a21846d9a5bf9a
Nguyen Van Dat <82921584+vandatnguyenn@users.noreply.github.com>
2022-10-31 17:01:19 +0700

    react query

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,58 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 import SignIn from './components/signin.UI';
 import SignUp from './components/signup.UI';
 import Memes from './components/memes.UI';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
-              19120190
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/memes'}>
-                    Get Memes
-                  </Link>
-                </li>
-              </ul>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={'/sign-in'}>
+                19120190
+              </Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-in'}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-up'}>
+                      Sign up
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/memes'}>
+                      Get Memes
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Routes>
+                <Route exact path="/" element={<SignIn />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/memes" element={<Memes />} />
+              </Routes>
             </div>
           </div>
-        </nav>
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<SignIn />} />
-              <Route path="/sign-in" element={<SignIn />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/memes" element={<Memes />} />
-            </Routes>
-          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- client/src/components/memes.UI.js
+++ client/src/components/memes.UI.js
@@ -1,42 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import {useQuery} from 'react-query';
 
 function Memes() {
     const [index, setIndex] = useState(0);
-    const [images, setImages] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
 
     function getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 
     function reloadImages(){
-        let randomNumber = getRndInteger(0, images.length);
+        let randomNumber = getRndInteger(0, 100);
         setIndex(randomNumber);
     }
-
-    function getImages(){
-        fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                const gallery = [];
-                for(let i = 0; i < result.data.memes.length; i++){
-                    gallery.push(result.data.memes[i]);
-                }
-                setIsLoaded(true);
-                setImages(gallery);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-        )
-    }
-
-    useEffect(() => {
-        getImages();
-    },[]);
 
     const style = {
         display: "flex",
@@ -44,35 +19,35 @@ function Memes() {
         alignItems: "center"
     };
 
-    const gallery = {
-        float: "left",
-        width: "300px",
-        padding: "2px",
-    }
+    useEffect(() => {
+        getMemes();
+    }, []);
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } 
-    else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } 
-    else {
-        return (
-            <div>
-                <p style={style}>Click to reload images</p>
-                <ul style={style}>
-                    <button onClick={() => reloadImages()}>Reload</button>
-                </ul>
-                <ul>
-                    <div style={gallery}>
-                    {
-                        <img src={images[index].url} width="300px"></img>
-                    }
-                    </div>
-                </ul>
-            </div>
-        );
-    }
+    const getMemes = async () => {
+		const res = await fetch('https://api.imgflip.com/get_memes');
+		return res.json();
+	};
+	
+	const {data, error, isLoading} = useQuery('/memes', getMemes);
+
+	if (error) return <div>Request Failed</div>;
+	if (isLoading) return <div>Loading...</div>;
+	
+	return (
+        <div>
+            <p style={style}>Click to reload images</p>
+            <ul style={style}>
+                <button onClick={() => reloadImages()}>Reload</button>
+            </ul>
+            <ul>
+                <div>
+                {
+                    <img src={data.data.memes[index].url} width="300px"></img>
+                }
+                </div>
+            </ul>
+        </div>
+	);
 }
 
 export default Memes;
